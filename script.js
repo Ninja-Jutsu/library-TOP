e4e2b0ef23a9682222b5c33fd499dcf9ed5b7fd6
Ninja-Jutsu <ninja-jutsu-way@gmail.com>
2023-10-30 19:11:21 +0800

    Add button that appends divs to the main section

--- script.js
+++ script.js
@@ -8,5 +8,35 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(){
-    
+    // Elements references:
+    var main = document.querySelector(".main")
+    var bookNameInput = document.querySelector("#book-name")
+    var authorNameInput = document.querySelector("#book-author")
+    var checkBoxInput = document.querySelector("#read")
+    // Elements creation:
+    var div = document.createElement("div")
+    div.classList.add("book");
+    var bookName = document.createElement ("p")
+    bookName.innerText = bookNameInput.value
+    var authorName = document.createElement ("p")
+    authorName.innerText = authorNameInput.value
+    var readButton = document.createElement("button")
+    readButton.innerText = 
+    div.appendChild(bookName)
+    div.appendChild(authorName)
+    main.appendChild(div);
+    document.querySelector("#popup").style.display = "none"
+    var inputList = document.querySelectorAll("input")
+    inputList[0].value = ""
+    inputList[1].value = ""
 }
+
+function openBook(){
+    document.querySelector("#popup").style.display = "block"
+}
+
+const popupButton = document.querySelector('.new-book')
+popupButton.addEventListener('click', openBook)
+
+const addButton = document.querySelector("#add-button")
+addButton.addEventListener("click", addBookToLibrary)
